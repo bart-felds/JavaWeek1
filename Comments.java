cddeda7aab2707c0908a078cd84f602a19b4589d
bart-felds <bart.fieldsiii@gmail.com>
2025-02-11 10:07:16 -0500

    update

---
+++ Comments.java
@@ -0,0 +1,15 @@
+/************************************
+*Project: Demo for Comments
+*Programer: Barrington Fields
+*Date: February, 2025
+*Program Name: Comments.java
+*************************************/
+
+public class Comments 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hi"); //message to user
+        System.out.println("LabOneA"); //assignment
+    }
+}
