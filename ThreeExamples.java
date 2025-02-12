f2b1ec71e82decb272bc3d3bc7058b7e6237b3a0
bart-felds <bart.fieldsiii@gmail.com>
2025-02-12 10:02:21 -0500

    three ex

--- ThreeExamples.java
+++ ThreeExamples.java
@@ -1,6 +1,24 @@
 public class ThreeExamples {
-    public static void main(String[]args)
-    {
-        System.out.println("\n\thello\\");
+    public static void main(String[] args) {
+        
+        System.out.println("\t\tDog Show Winners\n");
+        
+            
+      
+            
+        
+        
+        System.out.println("Place:\t1\t2\t3\t4\t5\t6");
+        System.out.println("Dog #:\t76\t32\t18\t47\t11\t29");
+        System.out.println("\tQ\"yz89\"Ja4\"tp");
+        System.out.println("\tAnderson\t3452");
+        System.out.println("\tBoyton\t\t5344");
+        System.out.println("\tCarrington\t3677");
+        System.out.println("\tHouston\t\t5988");
+        System.out.println("\tMcDougal\t5233");
+        System.out.println("\tRobertson\t3623");
+        System.out.println("\tStryker\t\t5112");
+
     }
 }
+
